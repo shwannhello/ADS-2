a8bda57ca9f0d4ade1c0ee4f4cd44ce01ef53181
shwannhello <shvarovaann@yandex.ru>
2026-03-16 00:06:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,58 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result = result * value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result = result * i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    double power = pown(x, n);
+    uint64_t factorial = fact(n);
+    if (factorial == 0) {
+        return 0.0;
+    }
+    return power / factorial;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 1.0;
+    double term = 1.0;
+    for (uint16_t n = 1; n < count; ++n) {
+        term = term * x / n;
+        result = result + term;
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = x;
+    double term = x;
+    for (uint16_t n = 1; n < count; ++n) {
+        term = term * (-1.0) * x * x;
+        term = term / ((2 * n) * (2 * n + 1));
+        result = result + term;
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 1.0;
+    double term = 1.0;
+    for (uint16_t n = 1; n < count; ++n) {
+        term = term * (-1.0) * x * x;
+        term = term / ((2 * n - 1) * (2 * n));
+        result = result + term;
+    }
+    return result;
 }
